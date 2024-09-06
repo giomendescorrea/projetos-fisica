63c85a91497a06ffca13d2184887e6e2986418b2
Giovanna Mendes Corrêa <147007498+giomendescorrea@users.noreply.github.com>
2024-09-06 19:27:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Autores do projeto: Giovanna Mendes, Guilherme Meireles, Ranielly Affonso e Natasha Almeida.")
+
+while True:
+    print("1. Bm - Campo Magnético")
+    print("2. Em - Campo Elétrico")
+    print("3. I - Intensidade")
+    entrada = int(input("Digite qual a entrada: "))
+
+    if entrada == 1:
+        print("FUNÇÃO 1")
+    elif entrada == 2:
+        print("FUNÇÃO 2")
+    elif entrada == 3:
+        print("FUNÇÃO 3")
+
+    
